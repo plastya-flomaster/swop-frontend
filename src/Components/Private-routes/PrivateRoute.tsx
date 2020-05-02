aa5205559a421de60a0fc4005f0e10430ce096d6
Dmitriy Putilov <dmitrii.98.putilov@gmail.com>
2020-05-02 20:43:41 +0500

    update private router

--- src/Components/Private-routes/PrivateRoute.tsx
+++ src/Components/Private-routes/PrivateRoute.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import MainPage from '../../Pages/MainPage';
+import UserPage from '../../Pages/UserPage';
+import HelpPage from '../../Pages/HelpPage';
+import EditUserPage from '../../Pages/EditUserPage';
+import { AppState } from '../../redux/Stores/store';
+import { getCategories } from '../../redux/Actions/CategoriesActions';
 
-const PrivateRoute: React.FC<any> = ({ component: Component, auth, ...rest }) => {
-    return (
-    <Route>
-        {auth.isAuthenticated === true ? (
-            <MainPage />
-        ) : <Redirect to='/login' />
-        }
-    </Route>)
+interface IPrivetRouter {
+  getCategories: () => void;
 }
 
-const mapStateToProps = (state: any) => ({
-    auth: state.auth
-});
+const PrivateRoute: React.FC<IPrivetRouter> = (props) => {
+  useEffect(() => {
+    props.getCategories();
+  }, []);
+  return (
+    <Switch>
+      <Route component={UserPage} path="/user" />
+      <Route component={HelpPage} path="/help" />
+      <Route component={EditUserPage} path="/edit" />
+      <Route exact component={MainPage} path="/swop" />
+    </Switch>
+  );
+};
 
-export default connect(mapStateToProps)(PrivateRoute);
+const mapStateToProps = (state: AppState) => ({});
+
+export default connect(mapStateToProps, { getCategories })(PrivateRoute);
