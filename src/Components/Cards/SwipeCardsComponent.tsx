5f64f9c1aa8fa00b538d8d3251e69f2beb76b130
Plastya <nastyapl98@gmail.com>
2020-05-02 21:45:47 +0500

    midded bracket

--- src/Components/Cards/SwipeCardsComponent.tsx
+++ src/Components/Cards/SwipeCardsComponent.tsx
@@ -9,38 +9,38 @@ interface Props {
   categories: ICategory[];
 }
 
-const SwipeCards: React.FC<Props> = ({ card, categories }) => 
-    <Box
-      round="small"
-      background={
-        card.photos && card?.photos?.length > 0
-          ? {
-              image: `url(${card?.photos[0]?.url && card?.photos[0]?.url})`,
-              dark: true,
-              opacity: 'medium',
-            }
-          : 'brand'
-      }
-      width="medium"
-      height="400px"
-      animation="slideUp"
-      pad="small"
-      justify="end"
-    >
-      <Text>{categories[card.category]}, г. Пермь</Text>
-      <Box>
-        <h1 className="title">{card.title}</h1>
-        <p className="text">{card.description}</p>
-        <Box align="start" justify="end">
-          <Box round={true} background="accent-2">
-            <Text margin={{ horizontal: 'small' }}>
-              {card.tags ? card.tags[0]?.tag : ''}
-            </Text>
-          </Box>
+const SwipeCards: React.FC<Props> = ({ card, categories }) => (
+  <Box
+    round="small"
+    background={
+      card.photos && card?.photos?.length > 0
+        ? {
+            image: `url(${card?.photos[0]?.url && card?.photos[0]?.url})`,
+            dark: true,
+            opacity: 'medium',
+          }
+        : 'brand'
+    }
+    width="medium"
+    height="400px"
+    animation="slideUp"
+    pad="small"
+    justify="end"
+  >
+    <Text>{categories[card.category]}, г. Пермь</Text>
+    <Box>
+      <h1 className="title">{card.title}</h1>
+      <p className="text">{card.description}</p>
+      <Box align="start" justify="end">
+        <Box round={true} background="accent-2">
+          <Text margin={{ horizontal: 'small' }}>
+            {card.tags ? card.tags[0]?.tag : ''}
+          </Text>
         </Box>
       </Box>
     </Box>
-  );
+  </Box>
+);
 
 const mapStateToProps = (state: AppState) => ({
   categories: state.categories.categories,
